7e40e8e095ff426d08796cd9170520e0ee51428b
Tom <tchom.asek@centrum.cz>
2020-11-03 19:17:08 +0100

    template tags

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,5 +4,18 @@ from django.shortcuts import render
 def home_view(request, *args,**kkwargs):
     return render(request, 'home.html', {})
 
-def contacts(*args,**kkwargs):
-    return HttpResponse('<h1>contacts</h2>')
+def about_view(request, *args,**kkwargs):
+    my_context = {
+        'title': 'this is about us',
+        "this_is_true": True,
+        'my_number': 123,
+        'my_list': [123,456,321,'abc'],
+        'my_html': "<h2>Hello world</h1>"
+    }
+    return render(request, 'about.html', my_context)
+
+def contact_view(request, *args,**kkwargs):
+    return render(request, 'contact.html', {})
+
+def social_view(request, *args,**kkwargs):
+    return HttpResponse('<h1>Social Page</h1>')
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, contacts
+from pages.views import home_view, contact_view, about_view, social_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('contacts', contacts, name='contacts'),
+    path('contact', contact_view,),
+    path('about', about_view,),
+    path('social', social_view),
 
 ]
